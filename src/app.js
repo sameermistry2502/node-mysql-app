d26590e9eb2ed1797392a4537d56ae0931025105
indianic <indianic@ind741.private>
2024-10-30 17:11:02 +0530

    update changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 app.use(express.json());
 
-app.use('/api', userRoutes);
+app.use('/api/v1', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
